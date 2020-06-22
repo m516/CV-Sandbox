2c91235ac0e8378174dd8115bbd5648ba673dc39
Micah <micah112358@gmail.com>
2020-06-21 21:11:31 -0500

    Create first project!
    Shows an image of a fox, but demonstrates that OpenCV can be found and included in projects!

---
+++ src/01-HelloWorld/CV-Sandbox.cpp
@@ -0,0 +1,31 @@
+﻿// CV-Sandbox.cpp : Defines the entry point for the application.
+//
+
+#include "CV-Sandbox.h"
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	// Print out a message of elation, now that OpenCV is installed!
+	cout << "Hello OpenCV!" << endl;
+
+	// Get an image name 
+	string filename = MEDIA_DIRECTORY;
+	// MEDIA_DIRECTORY is defined in the root-level CMake script
+	filename += "RedFox.png";
+
+	// Load the image
+	Mat image = imread(filename);
+	
+	// Show that image
+	imshow("Test image", image);
+
+	// Wait until the user is finished seeing the image.
+	waitKey(0);
+
+	//Now we're done.
+	return 0;
+}
