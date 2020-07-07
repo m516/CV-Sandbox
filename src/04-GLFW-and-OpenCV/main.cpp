435dfadbdbb7bfa2f96921df63aaedd332cb1c1d
Micah <micah112358@gmail.com>
2020-07-07 14:56:30 -0500

    Create Project 4
    Right now, this project is heavily based on a Gist that the Github user insaneyilin was very nice to share:
    https://gist.github.com/insaneyilin/038a022f2ece61c923315306ddcea081
    
    I will continue to tear this apart and make my own adjustments for a while.
    
    I still have a lot to learn...

---
+++ src/04-GLFW-and-OpenCV/main.cpp
@@ -0,0 +1,187 @@
+﻿#include <iostream>
+#include <string>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <opencv2/opencv.hpp>
+
+using std::cout;
+using std::endl;
+
+
+int window_width = 640;
+int window_height = 480;
+
+// Function turn a cv::Mat into a texture, and return the texture ID as a GLuint for use
+static GLuint matToTexture(const cv::Mat& mat, GLenum minFilter, GLenum magFilter, GLenum wrapFilter) {
+    // Generate a number for our textureID's unique handle
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+
+    // Bind to our texture handle
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    // Catch silly-mistake texture interpolation method for magnification
+    if (magFilter == GL_LINEAR_MIPMAP_LINEAR ||
+        magFilter == GL_LINEAR_MIPMAP_NEAREST ||
+        magFilter == GL_NEAREST_MIPMAP_LINEAR ||
+        magFilter == GL_NEAREST_MIPMAP_NEAREST)
+    {
+        cout << "You can't use MIPMAPs for magnification - setting filter to GL_LINEAR" << endl;
+        magFilter = GL_LINEAR;
+    }
+
+    // Set texture interpolation methods for minification and magnification
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
+
+    // Set texture clamping method
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapFilter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapFilter);
+
+    // Set incoming texture format to:
+    // GL_BGR       for CV_CAP_OPENNI_BGR_IMAGE,
+    // GL_LUMINANCE for CV_CAP_OPENNI_DISPARITY_MAP,
+    // Work out other mappings as required ( there's a list in comments in main() )
+    GLenum inputColourFormat = GL_BGR;
+    if (mat.channels() == 1)
+    {
+        inputColourFormat = GL_LUMINANCE;
+    }
+
+    // Create the texture
+    glTexImage2D(GL_TEXTURE_2D,     // Type of texture
+        0,                 // Pyramid level (for mip-mapping) - 0 is the top level
+        GL_RGB,            // Internal colour format to convert to
+        mat.cols,          // Image width  i.e. 640 for Kinect in standard mode
+        mat.rows,          // Image height i.e. 480 for Kinect in standard mode
+        0,                 // Border width in pixels (can either be 1 or 0)
+        inputColourFormat, // Input image format (i.e. GL_RGB, GL_RGBA, GL_BGR etc.)
+        GL_UNSIGNED_BYTE,  // Image data type
+        mat.ptr());        // The actual image data itself
+
+// If we're using mipmaps then generate them. Note: This requires OpenGL 3.0 or higher
+    if (minFilter == GL_LINEAR_MIPMAP_LINEAR ||
+        minFilter == GL_LINEAR_MIPMAP_NEAREST ||
+        minFilter == GL_NEAREST_MIPMAP_LINEAR ||
+        minFilter == GL_NEAREST_MIPMAP_NEAREST)
+    {
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
+    return textureID;
+}
+
+static void error_callback(int error, const char* description) {
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+static void resize_callback(GLFWwindow* window, int new_width, int new_height) {
+    glViewport(0, 0, window_width = new_width, window_height = new_height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, window_width, window_height, 0.0, 0.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+static void draw_frame(const cv::Mat& frame) {
+    // Clear color and depth buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);     // Operate on model-view matrix
+
+    glEnable(GL_TEXTURE_2D);
+    GLuint image_tex = matToTexture(frame, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR, GL_CLAMP);
+
+    /* Draw a quad */
+    glBegin(GL_QUADS);
+    glTexCoord2i(0, 0); glVertex2i(0, 0);
+    glTexCoord2i(0, 1); glVertex2i(0, window_height);
+    glTexCoord2i(1, 1); glVertex2i(window_width, window_height);
+    glTexCoord2i(1, 0); glVertex2i(window_width, 0);
+    glEnd();
+
+    glDeleteTextures(1, &image_tex);
+    glDisable(GL_TEXTURE_2D);
+}
+
+static void init_opengl(int w, int h) {
+    glViewport(0, 0, w, h); // use a screen size of WIDTH x HEIGHT
+
+    glMatrixMode(GL_PROJECTION);     // Make a simple 2D projection on the entire window
+    glLoadIdentity();
+    glOrtho(0.0, w, h, 0.0, 0.0, 100.0);
+
+    glMatrixMode(GL_MODELVIEW);    // Set the matrix mode to object modeling
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearDepth(0.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the window
+}
+
+int main(int argc, char** argv)
+{
+
+    // Get an image name 
+    std::string filename = MEDIA_DIRECTORY;
+    // MEDIA_DIRECTORY is defined in the root-level CMake script
+    filename += "RedFox.png";
+
+
+    cv::Mat img = cv::imread(filename);
+
+    window_width = img.cols;
+    window_height = img.rows;
+
+    GLFWwindow* window;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    window = glfwCreateWindow(window_width, window_height, "Simple example", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetWindowSizeCallback(window, resize_callback);
+
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+
+    //  Initialise glew (must occur AFTER window creation or glew will error)
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        cout << "GLEW initialisation error: " << glewGetErrorString(err) << endl;
+        exit(-1);
+    }
+    cout << "GLEW okay - using version: " << glewGetString(GLEW_VERSION) << endl;
+
+    init_opengl(window_width, window_height);
+
+    while (!glfwWindowShouldClose(window)) {
+        draw_frame(img);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    exit(EXIT_SUCCESS);
+}
