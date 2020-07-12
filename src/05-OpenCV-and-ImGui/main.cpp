4831d8800d044844f8273684b18ffc654bf3c0c4
Micah <micah112358@gmail.com>
2020-07-11 19:24:50 -0500

    Create working Project 5
    TODO create Readme

---
+++ src/05-OpenCV-and-ImGui/main.cpp
@@ -0,0 +1,298 @@
+﻿/* 
+* ImGui + OpenCV + GLFW
+*/
+
+#include "main.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw_gl3.h>
+
+#include <opencv2/opencv.hpp>
+
+#include <thread>
+
+using namespace std;
+using namespace cv;
+
+GLFWwindow* window;
+int window_width, window_height;
+
+// Function turn a cv::Mat into a texture, and return the texture ID as a GLuint for use
+static GLuint matToTexture(const cv::Mat& mat, GLenum minFilter, GLenum magFilter, GLenum wrapFilter) {
+	// Generate a number for our textureID's unique handle
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+
+	// Bind to our texture handle
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	// Catch silly-mistake texture interpolation method for magnification
+	if (magFilter == GL_LINEAR_MIPMAP_LINEAR ||
+		magFilter == GL_LINEAR_MIPMAP_NEAREST ||
+		magFilter == GL_NEAREST_MIPMAP_LINEAR ||
+		magFilter == GL_NEAREST_MIPMAP_NEAREST)
+	{
+		cout << "You can't use MIPMAPs for magnification - setting filter to GL_LINEAR" << endl;
+		magFilter = GL_LINEAR;
+	}
+
+	// Set texture interpolation methods for minification and magnification
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
+
+	// Set texture clamping method
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapFilter);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapFilter);
+
+	// Set incoming texture format to:
+	// GL_BGR       for CV_CAP_OPENNI_BGR_IMAGE,
+	// GL_LUMINANCE for CV_CAP_OPENNI_DISPARITY_MAP,
+	// Work out other mappings as required ( there's a list in comments in main() )
+	GLenum inputColourFormat = GL_BGR;
+	if (mat.channels() == 1)
+	{
+		inputColourFormat = GL_LUMINANCE;
+	}
+
+	// Create the texture
+	glTexImage2D(GL_TEXTURE_2D,     // Type of texture
+		0,                 // Pyramid level (for mip-mapping) - 0 is the top level
+		GL_RGB,            // Internal colour format to convert to
+		mat.cols,          // Image width  i.e. 640 for Kinect in standard mode
+		mat.rows,          // Image height i.e. 480 for Kinect in standard mode
+		0,                 // Border width in pixels (can either be 1 or 0)
+		inputColourFormat, // Input image format (i.e. GL_RGB, GL_RGBA, GL_BGR etc.)
+		GL_UNSIGNED_BYTE,  // Image data type
+		mat.ptr());        // The actual image data itself
+
+// If we're using mipmaps then generate them. Note: This requires OpenGL 3.0 or higher
+	if (minFilter == GL_LINEAR_MIPMAP_LINEAR ||
+		minFilter == GL_LINEAR_MIPMAP_NEAREST ||
+		minFilter == GL_NEAREST_MIPMAP_LINEAR ||
+		minFilter == GL_NEAREST_MIPMAP_NEAREST)
+	{
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+
+	return textureID;
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
+
+static void resize_callback(GLFWwindow* window, int new_width, int new_height) {
+	glViewport(0, 0, window_width = new_width, window_height = new_height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, window_width, window_height, 0.0, 0.0, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+static void init_opengl(int w, int h) {
+	glViewport(0, 0, w, h); // use a screen size of WIDTH x HEIGHT
+
+	glMatrixMode(GL_PROJECTION);     // Make a simple 2D projection on the entire window
+	glLoadIdentity();
+	glOrtho(0.0, w, h, 0.0, 0.0, 100.0);
+
+	glMatrixMode(GL_MODELVIEW);    // Set the matrix mode to object modeling
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearDepth(0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the window
+}
+
+/**
+ * A helper function for terminating the program
+ */
+void terminate(int errorCode) {
+	cout << "Closing application";
+	//Close GLFW
+	glfwTerminate();
+	//Exit
+	exit(errorCode);
+}
+
+
+/**
+ * A callback function for GLFW to execute when an internal error occurs with the
+ * library.
+ */
+void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+
+void setStyle(){
+	ImGuiStyle* style = &ImGui::GetStyle();
+	ImVec4* colors = style->Colors;
+
+	colors[ImGuiCol_Text] = ImVec4(0.92f, 0.92f, 0.92f, 1.00f);
+	colors[ImGuiCol_TextDisabled] = ImVec4(0.44f, 0.44f, 0.44f, 1.00f);
+	colors[ImGuiCol_WindowBg] = ImVec4(0.06f, 0.06f, 0.06f, 1.00f);
+	colors[ImGuiCol_PopupBg] = ImVec4(0.08f, 0.08f, 0.08f, 0.94f);
+	colors[ImGuiCol_Border] = ImVec4(0.51f, 0.36f, 0.15f, 1.00f);
+	colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+	colors[ImGuiCol_FrameBg] = ImVec4(0.11f, 0.11f, 0.11f, 1.00f);
+	colors[ImGuiCol_FrameBgHovered] = ImVec4(0.51f, 0.36f, 0.15f, 1.00f);
+	colors[ImGuiCol_FrameBgActive] = ImVec4(0.78f, 0.55f, 0.21f, 1.00f);
+	colors[ImGuiCol_TitleBg] = ImVec4(0.51f, 0.36f, 0.15f, 1.00f);
+	colors[ImGuiCol_TitleBgActive] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
+	colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.00f, 0.00f, 0.00f, 0.51f);
+	colors[ImGuiCol_MenuBarBg] = ImVec4(0.11f, 0.11f, 0.11f, 1.00f);
+	colors[ImGuiCol_ScrollbarBg] = ImVec4(0.06f, 0.06f, 0.06f, 0.53f);
+	colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.21f, 0.21f, 0.21f, 1.00f);
+	colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.47f, 0.47f, 0.47f, 1.00f);
+	colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.81f, 0.83f, 0.81f, 1.00f);
+	colors[ImGuiCol_CheckMark] = ImVec4(0.78f, 0.55f, 0.21f, 1.00f);
+	colors[ImGuiCol_SliderGrab] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
+	colors[ImGuiCol_SliderGrabActive] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
+	colors[ImGuiCol_Button] = ImVec4(0.51f, 0.36f, 0.15f, 1.00f);
+	colors[ImGuiCol_ButtonHovered] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
+	colors[ImGuiCol_ButtonActive] = ImVec4(0.78f, 0.55f, 0.21f, 1.00f);
+	colors[ImGuiCol_Header] = ImVec4(0.51f, 0.36f, 0.15f, 1.00f);
+	colors[ImGuiCol_HeaderHovered] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
+	colors[ImGuiCol_HeaderActive] = ImVec4(0.93f, 0.65f, 0.14f, 1.00f);
+	colors[ImGuiCol_ResizeGrip] = ImVec4(0.21f, 0.21f, 0.21f, 1.00f);
+	colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.91f, 0.64f, 0.13f, 1.00f);
+	colors[ImGuiCol_ResizeGripActive] = ImVec4(0.78f, 0.55f, 0.21f, 1.00f);
+	colors[ImGuiCol_PlotLines] = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
+	colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
+	colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+	colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+	colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
+
+	style->FramePadding = ImVec2(4, 2);
+	style->ItemSpacing = ImVec2(10, 2);
+	style->IndentSpacing = 12;
+	style->ScrollbarSize = 10;
+
+	style->WindowRounding = 4;
+	style->FrameRounding = 4;
+	style->ScrollbarRounding = 6;
+	style->GrabRounding = 4;
+
+	style->WindowTitleAlign = ImVec2(1.0f, 0.5f);
+
+	style->DisplaySafeAreaPadding = ImVec2(4, 4);
+
+}
+
+void setGuiScale(float guiScale) {
+	int fbw, fbh, ww, wh;
+	glfwGetFramebufferSize(window, &fbw, &fbh);
+	glfwGetWindowSize(window, &ww, &wh);
+
+	float pixelRatio = fbw / ww;
+
+	ImGui::GetIO().FontGlobalScale = guiScale / pixelRatio;
+}
+
+int main()
+{
+	//Attempt to initialize GLFW
+	if (!glfwInit())
+	{
+		//Initialization failed
+		cerr << "GLFW initialization failed :(";
+		//Use the terminate() function to safely close the application
+		terminate(1);
+	}
+
+	//Set the requirements for the version of OpenGL to use
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //Reqired for Mac OS X
+
+	//Set GLFW's error callback function
+	glfwSetErrorCallback(error_callback);
+
+	//GLFW creates a window and its OpenGL context with the next function
+	window = glfwCreateWindow(640, 480, "Hello window :)", NULL, NULL);
+
+	//Check for errors (which would happen if creating a window fails
+	if (!window)
+	{
+		// Window or OpenGL context creation failed
+		cerr << "GLFW failed to create a window and/or OpenGL context :(";
+		//Use the terminate() function to safely close the application
+		terminate(1);
+	}
+
+	//Window creation was successful. Continue
+	glfwMakeContextCurrent(window);
+
+	glewInit();
+
+	// Setup ImGui binding
+	ImGui_ImplGlfwGL3_Init(window, true);
+	setStyle();
+
+	bool show_test_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImColor(60, 55, 15);
+
+	//Set scale based on scale of monitor
+	GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+	float scale = 2.f;
+	glfwGetMonitorContentScale(monitor, &scale, nullptr);
+
+	// Load the image
+	// Get an image name 
+	string filename = MEDIA_DIRECTORY;
+	// MEDIA_DIRECTORY is defined in the root-level CMake script
+	filename += "RedFox.png";
+	// Load the image
+	const Mat image = imread(filename);
+	GLuint image_texture = matToTexture(image, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR, GL_CLAMP);
+
+	// The render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		ImGui_ImplGlfwGL3_NewFrame();
+
+		// ImGui::SetNextWindowSize(ImVec2(320,240));
+		ImGui::Begin("Another Window", &show_another_window);
+		ImGui::Text("Hello");
+
+		// Gui rendering size
+		ImGui::SliderFloat("Display scale", &scale, 1, 3);
+		setGuiScale(scale);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+		ImGui::End();
+
+		// Place the texture in an ImGui image
+		ImGui::Begin("OpenGL Texture Text");
+		ImVec2 imageSize = ImVec2(scale * image.size().width, scale * image.size().height);
+		ImGui::Text("size = %.0f x %.0f", imageSize.x, imageSize.y);
+		ImGui::Image((void*)(intptr_t)image_texture, imageSize);
+		ImGui::End();
+
+		// Rendering
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui::Render();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		std::this_thread::sleep_for(std::chrono::milliseconds(20));
+	}
+
+	// Cleanup
+	ImGui_ImplGlfwGL3_Shutdown();
+	//Close GLFW
+	glfwTerminate();
+	return 0;
+}
