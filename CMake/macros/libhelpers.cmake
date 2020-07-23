2541dd6b9e7abd1d41465785f2b09130a72b85b4
Micah <micah112358@gmail.com>
2020-07-23 13:46:07 -0500

    CUDA and OpenCV are optional now, but some projects just won't build without them

---
+++ CMake/macros/libhelpers.cmake
@@ -0,0 +1,15 @@
+macro (disable_if_cuda_not_found)
+    #Check if CUDA is unavailable
+    if(NOT CMAKE_CUDA_COMPILER)
+        message("${BoldRed}    Project can't be built without CUDA${ColorReset}")
+        return()
+    endif()
+endmacro()
+
+macro (disable_if_opencv_not_found)
+    #Check if CUDA is unavailable
+    if(NOT OpenCV_FOUND)
+        message("${BoldRed}    Project can't be built without OpenCV${ColorReset}")
+        return()
+    endif()
+endmacro()
