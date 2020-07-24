608afa2f568fcdb9068654380207a369ec606931
m516 <micah112358@gmail.com>
2020-07-23 21:41:19 -0500

    Run the setup script

--- setup-jetson-nano.sh
+++ setup-jetson-nano.sh
@@ -22,12 +22,6 @@ git clone https://github.com/Kitware/CMake.git
 cd CMake
 ./bootstrap && make && sudo make install
 
-# vcpkg
-#cd .tools/
-#sudo apt-get -y install curl unzip tar ninja-buid
-#git clone https://github.com/microsoft/vcpkg
-#cd vcpkg
-#./bootstrap-vcpkg.sh --useSystemBinaries -disableMetrics
 
 
 
